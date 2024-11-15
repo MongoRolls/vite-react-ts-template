752e3892fd3a0bca6389c11dafc490442e9d725c
xzc <xuzhichao1618@qq.com>
2024-11-15 16:19:15 +0800

    feat: 增加dockerfile文件 & 更新vite配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import path from 'path';
 
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+	plugins: [react()],
+	server: {
+		open: true
+	},
+	resolve: {
+		alias: {
+			'@': path.resolve(__dirname, './src')
+		}
+	},
+	css: {
+		preprocessorOptions: {
+			less: {
+				// less 配置项
+				javascriptEnabled: true
+			}
+		}
+	}
+});
